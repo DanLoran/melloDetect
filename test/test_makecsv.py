a9271380292710ec0caf9ae888a5d27e9a6a9cb7
Sam Sobell <sam.sobell@gmail.com>
2020-05-25 13:12:31 -0400

    Test fix (#22)
    * Fix tests
    
    * use pyfakefs

--- test/test_makecsv.py
+++ test/test_makecsv.py
@@ -1,11 +1,11 @@
 from ..dataset.makecsv import MakeCSV
 
-def test_MakeCsvEmptyList():
+def test_MakeCsvEmptyList(fs):
     imageList = ['img1', 'img2']
     labelList = []
     assert MakeCSV(labelList, imageList) == 0
 
-def test_MakeCSVWrongFormat():
+def test_MakeCSVWrongFormat(fs):
     imageList = ['img1', 'img2']
     labelList = ['l1', 'l2']
     assert MakeCSV(labelList, imageList) == 0
